15d8a419b1868106aa94fa0fada14de4807aed96
kazuya-supercell <50536150+kazuya-supercell@users.noreply.github.com>
2020-07-05 11:01:09 +0900

    Initial commit

---
+++ CountApplication/ViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: Step1. Main.storyboardからUILabelを初期化するコード
+    
+    // MARK: Step2. カウントしている数字を格納しておく変数を宣言しよう。
+    
+    // MARK: Step3. viewDidLoadって何だろう？下のコメントに知っていることを書いてみよう。
+    /*　[ここにviewDidLoadの説明を書いてね]
+     
+     */
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: Step4. plusメソッドはいつ呼ばれるのかな？Storyboardを確認しながら考えてみよう。
+    @IBAction func plus() {
+        // MARK: Step5. ここにはどんな処理を書けばいいのだろう？ネットで調べながら自分で書いてみよう。
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CountApplication
+チェックワーク 2020.7.5 課題アプリ①
